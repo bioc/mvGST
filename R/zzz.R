92d3fef360cfea277dcca0b470531bed71d4376d
lshep <shepherl@gmail.com>
2018-03-05 12:06:00 -0500

    Update mvGST
       - don't deprecate as not following phase out procedure
       - add message that it is being removed

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,7 @@
 .onAttach <- function(libname, pkgname) {
     msg <- sprintf(
-        "Package '%s' is deprecated and will be removed from Bioconductor
-         version %s", pkgname, "3.7")
-    .Deprecated(msg=paste(strwrap(msg, exdent=2), collapse="\n"))
+        "Package '%s' is being removed from Bioconductor.
+         Starting with Bioc %s package should be available on CRAN",
+        pkgname, "3.7")
+    packageStartupMessage(msg=paste(strwrap(msg, exdent=2), collapse="\n"))
 }
